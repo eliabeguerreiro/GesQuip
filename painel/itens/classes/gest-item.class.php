04a9a57f418e636e3f6b3e8e84b9353637dbf4d8
Eliabe G da Paz <59834090+eliabeguerreiro@users.noreply.github.com>
2025-02-26 13:00:05 -0300

    Update classes
    corrigindo nomes e duplicidade

--- painel/itens/classes/gest-item.class.php
+++ painel/itens/classes/gest-item.class.php
@@ -6,6 +6,109 @@ class Item
 {
 
 
+    public static function reservaItem($id, $mov){    
+        $db = DB::connect();
+
+        $rs = $db->prepare("SELECT id_responsavel FROM movimentacao WHERE id_movimentacao = $mov");
+        $rs->execute();
+        $resultado = $rs->fetchAll(PDO::FETCH_ASSOC);
+        $id_responsavel = ($resultado[0]['id_responsavel']);
+
+        $rs = $db->prepare(query: "SELECT nv_permissao FROM usuarios WHERE id_usuario = $id_responsavel");
+        $rs->execute();
+        $resultado = $rs->fetchAll(PDO::FETCH_ASSOC);
+        $nv_perm_user = ($resultado[0]['nv_permissao']);
+
+        $rs = $db->prepare("SELECT nv_permissao FROM item WHERE id_item = $id");
+        $rs->execute();
+        $resultado = $rs->fetchAll(PDO::FETCH_ASSOC);
+        $nv_perm_item = ($resultado[0]['nv_permissao']);
+
+        if($nv_perm_item > $nv_perm_user){
+            $_SESSION['msg'] = 'AVISO: NÍVEL DO ITEM É MAIOR QUE O DO FUNCIONÁRIO!';
+        }
+
+        $rs = $db->prepare("UPDATE item SET nr_disponibilidade = $mov WHERE id_item = $id");
+        $rs->execute();
+        $rows = $rs->rowCount();
+        if ($rows > 0){
+            return true;
+        }
+        
+    }
+
+
+
+
+    public static function devolverItem($id_item, $id_movimentacao){
+        $dt = date('Y-m-d H:i:s');
+        $db = DB::connect();
+        
+        // primeiro eu indentifico o id_item_movimentacao que está associado ao item e a movimentação
+        
+        $rs = $db->prepare("SELECT id_item_movimentacao FROM item_movimentacao WHERE id_item = $id_item and id_movimentacao = $id_movimentacao");
+        $rs->execute();
+        $resultado = $rs->fetchAll(PDO::FETCH_ASSOC);
+        $id_item_mov = $resultado[0]['id_item_movimentacao'];
+        
+
+        // depois eu atualizo a data de devolução do item_movimentacao
+        $rs = $db->prepare("UPDATE item_movimentacao SET dt_devolucao = '$dt' WHERE id_item_movimentacao = $id_item_mov");
+        $rs->execute();
+        $rows = $rs->rowCount();
+
+        if($rows > 0){ 
+            echo '<script>';
+            echo 'console.log("Tabela item-movimento atualizada");';
+            echo '</script>';
+                       
+            // depois eu atualizo a disponibilidade do item
+
+            $rs = $db->prepare("UPDATE item SET nr_disponibilidade = 1 WHERE id_item = $id_item");
+            $rs->execute();
+            $rows = $rs->rowCount();
+
+            if($rows > 0){  
+
+                echo '<script>';
+                echo 'console.log("Disponibilidade atualizada");';
+                echo '</script>';
+                return true;
+            }
+        }
+
+    }
+
+
+    public static function getItensReservados($id){
+        
+        $db = DB::connect();
+        $rs = $db->prepare("SELECT * FROM item WHERE nr_disponibilidade = $id");
+        $rs->execute();
+        $resultado = $rs->fetchAll(PDO::FETCH_ASSOC);
+        return ["dados" => $resultado];
+
+    }
+
+    public static function getItensDevolvidos($id){
+        
+        $db = DB::connect();
+        $rs = $db->prepare("SELECT * FROM item_movimentacao WHERE id_movimentacao = $id and dt_devolucao IS NOT NULL ");
+        $rs->execute();
+        $rows = $rs->rowCount();
+
+        if($rows > 0){ 
+            $resultado = $rs->fetchAll(PDO::FETCH_ASSOC);
+            return ["dados" => $resultado];
+
+        }else{
+            return ["dados" => null];
+
+        }
+        
+    }
+
+
     public static function devolverItemByPanel($id_item){
         $dt = date('Y-m-d H:i:s');
         $db = DB::connect();

3cdaf209a0ce336b6c897f65dac960effa7b4bf9
Eliabe G da Paz <59834090+eliabeguerreiro@users.noreply.github.com>
2025-03-11 16:26:45 -0300

    update moviment
    corrigindo front

--- painel/moviment/src/script.js
+++ painel/moviment/src/script.js
@@ -1,27 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const filtroPrincipal = document.getElementById("filtro_principal");
-    const divNatureza = document.getElementById("filtro_natureza");
-    const divFamilia = document.getElementById("filtro_familia");
-
-    // Função para alternar a visibilidade das divs
-    filtroPrincipal.addEventListener("change", function () {
-        const valorSelecionado = filtroPrincipal.value;
-
-        // Oculta ambas as divs inicialmente
-        divNatureza.style.display = "none";
-        divFamilia.style.display = "none";
-
-        // Exibe a div correspondente à opção selecionada
-        if (valorSelecionado === "natureza") {
-            divNatureza.style.display = "inline-block";
-        } else if (valorSelecionado === "familia") {
-            divFamilia.style.display = "inline-block";
-        }
-    });
-});
-
-
-
 const finalizaButtons = document.querySelectorAll('.atualiza-button');
 const modal = document.getElementById('atualizaModal');
 const closeModal = document.querySelector('.modal .close');

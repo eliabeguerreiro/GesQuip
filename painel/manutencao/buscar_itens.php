79be851eb79c632821b2d898ade4cdb2f30c4fba
Eliabe G da Paz <59834090+eliabeguerreiro@users.noreply.github.com>
2025-03-26 15:30:38 -0300

    update manutenc

--- painel/manutencao/buscar_itens.php
+++ painel/manutencao/buscar_itens.php
@@ -5,7 +5,7 @@ include_once "classes/gest-manutencao.class.php";
 $query = isset($_GET['q']) ? $_GET['q'] : '';
 
 $db = DB::connect();
-$rs = $db->prepare("SELECT DISTINCT * FROM item WHERE nr_disponibilidade = 1 and ds_item LIKE :query OR cod_patrimonio LIKE :query");
+$rs = $db->prepare("SELECT DISTINCT * FROM item WHERE desativado is null and nr_disponibilidade = 1 and ds_item LIKE :query OR cod_patrimonio LIKE :query order by id_item asc");
 $rs->bindValue(':query', '%' . $query . '%', PDO::PARAM_STR);
 $rs->execute();
 $itens = $rs->fetchAll(PDO::FETCH_ASSOC);
--- painel/manutencao/classes/gest-manutencao.class.php
+++ painel/manutencao/classes/gest-manutencao.class.php
@@ -20,10 +20,10 @@ class Manutencao
 
         //echo("SELECT id_item_movimentacao FROM item_movimentacao WHERE id_item = $id_item");
        
-        $rs = $db->prepare("SELECT id_item_movimentacao FROM item_movimentacao WHERE id_item = $id_item ORDER BY id_item_movimentacao DESC LIMIT 1");
+        $rs = $db->prepare("SELECT * FROM item_movimentacao WHERE id_item = $id_item ORDER BY id_item_movimentacao DESC LIMIT 1");
         $rs->execute();
         $resultado = $rs->fetchAll(PDO::FETCH_ASSOC);
-        $id_it_mov = $resultado[0]['id_item_movimentacao'];
+        $id_mov = $resultado[0]['id_movimentacao'];
 
 
         //echo("UPDATE item_movimentacao SET dt_devolucao = '$dt' WHERE id_item_movimentacao = $id_it_mov");
@@ -34,7 +34,7 @@ class Manutencao
         if($rows > 0){ 
 
                 $rs = $db->prepare("INSERT INTO manutencao (id_item, id_item_movimentacao, id_autor, obs_in)
-                VALUES(".$id_item.",".$id_it_mov.",'".$user['id_usuario']."','$texto')");
+                VALUES(".$id_item.",".$id_mov.",'".$user['id_usuario']."','$texto')");
                 $rs->execute();
                 $rows = $rs->rowCount();
                 
@@ -93,10 +93,10 @@ class Manutencao
                 if ($nm_filtro == 'dt_movimentacao') {
                     if (strpos($filtro, '...') !== false) {
                         list($start, $end) = explode('...', $filtro);
-                        $rs = $db->prepare("SELECT * FROM manutencao WHERE dt_inicio_manutencao BETWEEN '$start' AND '$end' AND dt_fim_manutencao IS NULL ORDER BY dt_inicio_manutencao DESC");
+                        $rs = $db->prepare("SELECT * FROM manutencao WHERE dt_inicio_manutencao BETWEEN '$start' AND '$end' AND dt_fim_manutencao IS NULL ORDER BY id_manutencao DESC");
                     }
                 } else {
-                    $rs = $db->prepare("SELECT * FROM manutencao WHERE $nm_filtro = '$filtro' AND dt_fim_manutencao IS NULL ORDER BY dt_inicio_manutencao DESC");
+                    $rs = $db->prepare("SELECT * FROM manutencao WHERE $nm_filtro = '$filtro' AND dt_fim_manutencao IS NULL ORDER BY id_manutencao DESC");
                 }
                 $rs->execute();
                 $resultado = $rs->fetchAll(PDO::FETCH_ASSOC);
@@ -104,7 +104,7 @@ class Manutencao
 
             }else{
                 
-                $rs = $db->prepare("SELECT * FROM manutencao WHERE dt_fim_manutencao IS NULL ORDER BY dt_inicio_manutencao DESC");
+                $rs = $db->prepare("SELECT * FROM manutencao WHERE dt_fim_manutencao IS NULL ORDER BY id_manutencao DESC");
                 $rs->execute();
                 $resultado = $rs->fetchAll(PDO::FETCH_ASSOC);
                 return ["dados" => $resultado];
@@ -121,16 +121,16 @@ class Manutencao
             if ($nm_filtro == 'dt_movimentacao') {
                 if (strpos($filtro, '...') !== false) {
                     list($start, $end) = explode('...', $filtro);
-                    $rs = $db->prepare("SELECT * FROM manutencao WHERE dt_inicio_manutencao BETWEEN '$start' AND '$end' AND dt_fim_manutencao IS NOT NULL ORDER BY dt_fim_manutencao DESC");
+                    $rs = $db->prepare("SELECT * FROM manutencao WHERE dt_inicio_manutencao BETWEEN '$start' AND '$end' AND dt_fim_manutencao IS NOT NULL ORDER BY id_manutencao DESC");
                 }
             } else {
-                $rs = $db->prepare("SELECT * FROM manutencao WHERE $nm_filtro = '$filtro' AND dt_fim_manutencao IS NOT NULL ORDER BY dt_fim_manutencao DESC");
+                $rs = $db->prepare("SELECT * FROM manutencao WHERE $nm_filtro = '$filtro' AND dt_fim_manutencao IS NOT NULL ORDER BY id_manutencao DESC");
             }
             $rs->execute();
             $resultado = $rs->fetchAll(PDO::FETCH_ASSOC);
             return ["dados" => $resultado];
         } else {
-            $rs = $db->prepare("SELECT * FROM manutencao WHERE dt_fim_manutencao IS NOT NULL ORDER BY dt_fim_manutencao DESC");
+            $rs = $db->prepare("SELECT * FROM manutencao WHERE dt_fim_manutencao IS NOT NULL ORDER BY id_manutencao DESC");
             $rs->execute();
             $resultado = $rs->fetchAll(PDO::FETCH_ASSOC);
             return ["dados" => $resultado];
@@ -147,11 +147,11 @@ class Manutencao
         $id_mov = $_SESSION['id_moviment'];
         $user = $_SESSION['data_user'];
        
-        $rs = $db->prepare("SELECT id_item_movimentacao FROM item_movimentacao WHERE id_item = $id_item and id_movimentacao = $id_mov");
+        $rs = $db->prepare("SELECT * FROM item_movimentacao WHERE id_item = $id_item and id_movimentacao = $id_mov");
         $rs->execute();
         $resultado = $rs->fetchAll(PDO::FETCH_ASSOC);
         $id_it_mov = $resultado[0]['id_item_movimentacao'];
-
+        $id_mov = $resultado[0]['id_movimentacao'];
      
         $rs = $db->prepare("UPDATE item SET nr_disponibilidade = 2 WHERE id_item = $id_item");
         $rs->execute();
@@ -173,7 +173,7 @@ class Manutencao
                 echo '</script>';
 
                 $rs = $db->prepare("INSERT INTO manutencao (id_item, id_item_movimentacao, id_autor, obs_in)
-                VALUES(".$id_item.",".$id_it_mov.",'".$user['id_usuario']."','".$_POST['descricao']."')");
+                VALUES(".$id_item.",".$id_mov.",'".$user['id_usuario']."','".$_POST['descricao']."')");
                 $rs->execute();
                 $rows = $rs->rowCount();
                 

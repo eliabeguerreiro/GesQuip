074601192469bf80e8f072b67d4bb13d2aa8a532
Eliabe G da Paz <eliabepaz.work@gmail.com>
2025-03-29 10:43:26 -0300

    Update Favicon

--- painel/moviment/classes/conteudo.painel-moviment.class.php
+++ painel/moviment/classes/conteudo.painel-moviment.class.php
@@ -11,6 +11,7 @@ class ContentPainelMoviment
           <meta charset="UTF-8">
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
           <title>GesQuip - Equipamentos</title>
+          <link rel="icon" type="image/png" href="src/img/favicon.png">
           <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
           <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
           <link rel="stylesheet" href="src/style.css">

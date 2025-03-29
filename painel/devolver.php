1411cb99c48f9398800b0469ee660955e000e035
Eliabe G da Paz <eliabepaz.work@gmail.com>
2025-03-29 00:49:29 -0300

    Update painel e item
    devolução direto do painel realizada

--- painel/devolver.php
+++ painel/devolver.php
@@ -1,26 +1,63 @@
 <?php
 session_start();
-include_once "classes/painel.class.php";
-include_once "itens/classes/item.class.php";
 include_once "classes/db.class.php";
 
-if (isset($_POST['id'])) {
-    $itemId = $_POST['id'];
 
-    if ($reserva = Item::devolverItemByPanel($itemId)) {
-        echo json_encode([
-            'success' => true,
-            'message' => 'Devolução realizada com sucesso!'
-        ]);
-    } else {
-        echo json_encode([
-            'success' => false,
-            'message' => 'Falha ao realizar a devolução.'
-        ]);
+    $id_item = $_POST['id'];
+
+
+
+        $dt = date('Y-m-d H:i:s');
+        $db = DB::connect();
+        
+        // primeiro eu indentifico o id_item_movimentacao que está associado ao item e a movimentação
+        
+        $rs = $db->prepare("SELECT * FROM item_movimentacao WHERE id_item = $id_item ORDER BY id_item_movimentacao DESC LIMIT 1");
+        $rs->execute();
+        $resultado = $rs->fetchAll(PDO::FETCH_ASSOC);
+        $id_item_mov = $resultado[0]['id_item_movimentacao'];
+        $id_mov = $resultado[0]['id_movimentacao'];
+        
+
+        // depois eu atualizo a data de devolução do item_movimentacao
+        $rs = $db->prepare("UPDATE item_movimentacao SET dt_devolucao = '$dt' WHERE id_item_movimentacao = $id_item_mov");
+        $rs->execute();
+        $rows = $rs->rowCount();
+
+        if($rows > 0){ 
+                       
+            // depois eu atualizo a disponibilidade do item
+
+            $rs = $db->prepare("UPDATE item SET nr_disponibilidade = 1 WHERE id_item = $id_item");
+            $rs->execute();
+            $rows = $rs->rowCount();
+
+            if($rows > 0){  
+
+                $rs = $db->prepare("SELECT * FROM item_movimentacao WHERE id_movimentacao = $id_mov and dt_devolucao is null");
+                $rs->execute();
+                $rows = $rs->rowCount();
+
+                if($rows == 0){ 
+
+                    // depois eu atualizo a data de devolução da movimentação
+                    $rs = $db->prepare("UPDATE movimentacao SET dt_finalizacao = '$dt' WHERE id_movimentacao = $id_mov");
+                    $rs->execute();
+                    $rows = $rs->rowCount();
+
+                    echo json_encode([
+                        'success' => true,
+                        'message' => 'Devolução realizada com sucesso - A movimentação foi Encerrada!'
+                    ]);
+
+                return true;
+            }else{
+
+                echo json_encode([
+                    'success' => true,
+                    'message' => 'Devolução realizada com sucesso - A movimentação foi Encerrada!'
+                ]);
+
+            }
+        }
     }
-} else {
-    echo json_encode([
-        'success' => false,
-        'message' => 'ID do item não fornecido.'
-    ]);
-}
